d0b97b0d999e8a35c918761dccd7a204f36be9a2
altf4m88 <fadhilmauladhani@gmail.com>
2021-06-23 10:52:07 +0700

    update profile seeder

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -16,13 +16,13 @@ class ProfileSeeder extends Seeder
     public function run()
     {
         DB::table('tbl_setting_lap')->insert([
-            'nama_perusahaan' => 'PT. Mepo Sejahtera',
+            'nama_perusahaan' => 'PT. Buku Jaya Makmur',
             'alamat' => 'Jl. Jalan',
-            'no_tlpn' => '666-0-78',
-            'web' => 'www.waggish-mepo.id',
-            'logo' => 'database.jpg',
-            'no_hp' => '08973273232',
-            'email' => 'mepo@mail.com',
+            'no_tlpn' => '666-0-555',
+            'web' => 'www.jamur.id',
+            'logo' => 'book.png',
+            'no_hp' => '0898374923',
+            'email' => 'jamur@mail.com',
             'updated_at' => Carbon::now(),
             'created_at' => Carbon::now(),
         ]);
